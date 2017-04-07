445b227a83759606786e929a64728ff86d9d3cf3
Jonathan Wang <a1005519@alumni.nuk.edu.tw>
2017-04-07 12:34:54 -0400

    Update CNN module

--- classifiers/convolu_net.py
+++ classifiers/convolu_net.py
@@ -1,10 +1,45 @@
-class convolu_net(object):
+import tensorflow as tf
 
-  def conv2d(self, input_data, weight, bias, stride):
-    raise NotImplementedError
 
-  def maxmaxpool2d(self, input_data, k):
-    raise NotImplementedError
 
-  def conv_net(self, input_data, weight, bias):
-    raise NotImplementedError
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def maxpool2d(x, k=2):
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
+
+
+# Create model
+def conv_net(x, weights, biases, dropout):
+    # Reshape input picture
+    x = tf.reshape(x, shape=[-1, 32, 32, 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+    l2_reg = tf.nn.l2_loss(weights['wc1'])
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    l2_reg = l2_reg + tf.nn.l2_loss(weights['wc2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    l2_reg = tf.nn.l2_loss(weights['wd1'])
+    # Apply Dropout
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out, l2_reg
