f03110fbc0d80b2deb928ad56d23433d2ad741db
Giang Nguyen <giang.ng.1994@gmail.com>
2017-04-17 14:05:48 -0400

    Added mouse movement tracking

--- pythonGUI.py
+++ pythonGUI.py
@@ -9,48 +9,73 @@ from PIL import ImageTk, Image
 import sys, os
 
 box=[0,0,0,0]
+boxes=[]
+master = Tk()
+startx,starty=0,0
+rec=0
+recs=[]
+mainCanvas = Canvas(master,width=600,height=600)
 photo=Image.new("RGB",(1000,1000),"white")
-imgName= None
+album=[]
+counter=0
 
 def motion(event):
+	global box,startx, starty
 	box[0],box[1]=event.x, event.y
+	startx,starty = event.x,event.y
 	print("Top left: (%s %s)" % (event.x, event.y))
 	return
 
-def mouseRelease(event):
-	box[2],box[3]=event.x,event.y
-	print("Top right: (%s %s)" % (event.x,event.y))
+def mouseMotion(event):
+	global box, boxes, rec
+	mainCanvas.delete(rec)
+	rec=mainCanvas.create_rectangle(startx,starty,event.x,event.y)
+	rec_coor = [startx,starty,event.x,event.y]
+	recs.append(rec_coor)
 	return
-	
+
+def mouseRelease(event):
+	global box, boxes, rec,recs, counter
+	box[2],box[3]=event.x,event.y
+	boxes.append(box)
+	mainCanvas.create_rectangle(boxes[counter][0],boxes[counter][1],
+		boxes[counter][2],boxes[counter][3])
+	box=[0,0,0,0]
+	counter+=1
+	print counter
+	print("Top right: (%s %s)" % (event.x,event.y))
+	print boxes
+	return
+
 def keyDown(event):
+	global album
 	print event.char
-	rect = (box[0],box[1],box[2],box[3])
-	print rect
-	try:
-		cropImg = photo.crop(rect)
-		if cropImg.size[0]==0 and cropImg.size[1]==0:
-			raise SystemError("Nothing to crop")
-		cropImg.save(imgName+".jpg")
-		print("donezo")
-	except SystemError as e:
-		print("something wong")
-		pass
+	for b in boxes:
+		rect = (b[0],b[1],b[2],b[3])
+		print rect
+		try:
+			cropImg = photo.crop(rect)
+			if cropImg.size[0]==0 and cropImg.size[1]==0:
+				raise SystemError("Nothing to crop")
+			cropImg.save(str(boxes.index(b))+".jpg")
+			album.append(cropImg)
+			print("done")
+		except SystemError as e:
+			print("something wong")
+			pass
+	print album
 
 def binding(filename):
 	global photo
-	global imgName
-	master = Tk()
-	#master.minsize(width=500,height=500)
 	photo = Image.open(filename)
-	filename=filename.split(".")[0]
-	imgName=filename+"_cropped"
-	# print photo.size
 	img = ImageTk.PhotoImage(photo)
-	panel = Label(master,image=img)
+	mainCanvas.config(width=photo.size[0],height=photo.size[1])
+	mainCanvas.create_image(photo.size[0]/2,photo.size[1]/2,image=img)
 	master.bind('<Button-1>',motion)
+	master.bind('<Button1-Motion>',mouseMotion)
 	master.bind('<ButtonRelease-1>',mouseRelease)
 	master.bind('<Key>',keyDown)
-	panel.pack(expand="yes")
+	mainCanvas.pack(expand="yes")
 	mainloop()
 
 if os.path.isfile(sys.argv[1]):
